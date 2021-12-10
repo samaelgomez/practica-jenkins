148dd61a1b8c4b6cc9f220455b62d61e48e942e7
samaelgomez <samaelgomezherrera@gmail.com>
2021-12-10 16:38:03 +0100

    Lint detected errors solved

--- pages/api/users/index.js
+++ pages/api/users/index.js
@@ -4,15 +4,14 @@ export default handler;
 
 function handler(req, res) {
   switch (req.method) {
-    case 'GET':
+    case "GET":
       return getUsers();
-    case 'POST0':
+    case "POST":
       return createUser();
-    default:
-        return res.status(405).end(`Method ${req.method} Not Allowed`);
-    case 'DELETE':
+    case "DELETE":
       return deleteAllUsers();
-
+    default:
+      return res.status(405).end(`Method ${req.method} Not Allowed`);
   }
 
   function getUsers() {
