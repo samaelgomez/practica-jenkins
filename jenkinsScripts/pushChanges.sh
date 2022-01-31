8b3b248b7171e26a22c7f9c3d9cf77835d8b2baf
samaelgomez <samaelgomezherrera@gmail.com>
2022-01-31 21:53:35 +0100

    Push changes script test

--- jenkinsScripts/pushChanges.sh
+++ jenkinsScripts/pushChanges.sh
@@ -1,4 +1,5 @@
 git config user.email "samaelgomezherrera@gmail.com"
 git config user.name "samaelgomez"
 git add .
-if [ git diff --cached --exit-code == 0 ]; then git commit -m "Pipeline ejecutada por " $1 ". Motivo: " $2; git push origin master; fi
+git commit -m "Pipeline ejecutada por $1 . Motivo: $2"
+git push origin master
