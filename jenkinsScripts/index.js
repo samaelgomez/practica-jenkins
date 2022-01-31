17538b7780226f72744493a200b03e06c5be36f8
samaelgomez <samaelgomezherrera@gmail.com>
2022-01-31 21:47:38 +0100

    fs import syntax change

--- jenkinsScripts/index.js
+++ jenkinsScripts/index.js
@@ -1,13 +1,13 @@
-import { readFileSync, writeFileSync } from "fs";
+var fs = require("fs");
 
 const cypressResult = process.argv[2];
-const fileContent = readFileSync("./README.md", "utf8");
+const fileContent = fs.readFileSync("./README.md", "utf8");
 const regex = new RegExp(/<!---Start place for the badge -->\n(.*)\n<!---End place for the badge -->/g);
 
 if (cypressResult == "0") {
     const replaceBadge = fileContent.replace(regex, "<!---Start place for the badge -->\nRESULTADO DE LOS ÚLTIMOS TESTS: [![Cypress.io](https://img.shields.io/badge/tested%20with-Cypress-04C38E.svg)](https://www.cypress.io/)\n<!---End place for the badge -->");
-    writeFileSync("./README.md", replaceBadge);
+    fs.writeFileSync("./README.md", replaceBadge);
 } else {
     const replaceBadge = fileContent.replace(regex, "<!---Start place for the badge -->\nRESULTADO DE LOS ÚLTIMOS TESTS: [![Cypress.io](https://img.shields.io/badge/test-failure-red)](https://www.cypress.io/)\n<!---End place for the badge -->");
-    writeFileSync("./README.md", replaceBadge);
+    fs.writeFileSync("./README.md", replaceBadge);
 }
