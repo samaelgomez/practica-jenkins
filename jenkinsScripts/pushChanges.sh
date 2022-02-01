4b7957432e63f9257280d3d15da5beb53c8e6bbd
samaelgomez <samaelgomezherrera@gmail.com>
2022-02-01 06:36:43 +0100

    Push file update

--- jenkinsScripts/pushChanges.sh
+++ jenkinsScripts/pushChanges.sh
@@ -2,4 +2,4 @@ git config user.email "samaelgomezherrera@gmail.com"
 git config user.name "samaelgomez"
 git add .
 git commit -m "Pipeline ejecutada por $1 . Motivo: $2"
-git push origin master
+git push origin HEAD:master
